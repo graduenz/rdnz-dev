9a87595750336e856de7096c41bf17cad5ce93a5
Guilherme Augusto Raduenz <graduenz@gmail.com>
2023-05-08 14:57:54 -0300

    fix: disabled slice-simulator page

--- pages/slice-simulator.vue
+++ pages/slice-simulator.vue
@@ -1,10 +1,10 @@
 <template>
   <SliceSimulator #default="{ slices }">
-    <SliceZone :slices="slices" :components="components" />
+    <!-- <SliceZone :slices="slices" :components="components" /> -->
   </SliceSimulator>
 </template>
 
 <script setup lang="ts">
 import { SliceSimulator } from "@slicemachine/adapter-nuxt/simulator";
-import { components } from "~/slices";
+// import { components } from "~/slices";
 </script>
