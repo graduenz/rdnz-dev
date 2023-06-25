7c5d50c313ad4d9048c54ff9786713a9bc5706c9
Guilherme Raduenz <graduenz@gmail.com>
2023-06-25 10:31:19 -0300

    style: decreased page title sizes

--- components/PageTitle.vue
+++ components/PageTitle.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1 class="scroll-m-16 text-6xl font-bold  tracking-tighter whitespace-nowrap text-gray-800 dark:text-gray-200">
+    <h1 class="scroll-m-16 text-5xl font-bold  tracking-tighter whitespace-nowrap text-gray-800 dark:text-gray-200">
       {{ title }}
     </h1>
     <h2 class="pt-2 text-gray-500 text-lg">
