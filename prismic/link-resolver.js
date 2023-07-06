b45f9f6352f9f18106adeaf74a0690d313081788
Guilherme Raduenz <graduenz@gmail.com>
2023-07-06 12:07:45 -0300

    fix!: removed everything before starting new project

--- prismic/link-resolver.js
+++
@@ -1,7 +0,0 @@
-export default function linkResolver (doc) {
-  if (doc.type === 'article') {
-    return `/blog/${doc.uid}`;
-  }
-
-  return null;
-}
--- public/gtag.js
+++
@@ -1,5 +0,0 @@
-window.dataLayer = window.dataLayer || [];
-// eslint-disable-next-line no-undef
-function gtag() { dataLayer.push(arguments); }
-gtag('js', new Date());
-gtag('config', 'G-5KTNBB50LM');
